d351422bfee05f43e75dda2ade162ed21bae1dc3
Bennie van der Walt <barendjohannes.vdwalt@gmail.com>
2018-10-10 14:58:46 +0200

    Hello world in C#

---
+++ Program/hello_world_bvdwalt.cs
@@ -0,0 +1,16 @@
+// LANGUAGE: C#
+// AUTHOR: Bennie van der Walt
+// GITHUB: https://github.com/bvdwalt
+
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
