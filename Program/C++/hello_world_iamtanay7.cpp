b69813b87df2ba9c10724531406de585cbd9a949
Tanay Shirodkar <tanayshirodkar0@gmail.com>
2018-11-12 08:46:12 +0530

    Add hello world program in C++

---
+++ Program/C++/hello_world_iamtanay7.cpp
@@ -0,0 +1,12 @@
+
+// LANGUAGE: C++
+// AUTHOR: Tanay Shirodkar
+// GITHUB: https://github.com/iamtanay7
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello, World!"<<endl;
+	return 0;
+}
