70cd0a9142ae0edcd06d921e2aa2ca20cfacb4a6
Parth Vyas <30977206+parthvyas7@users.noreply.github.com>
2019-10-20 15:33:09 +0530

    Create hello_world_parthvyas7.cs

---
+++ Program/C#/hello_world_parthvyas7.cs
@@ -0,0 +1,16 @@
+// LANGUAGE: C#
+// AUTHOR: Parth Vyas
+// GITHUB: https://github.com/parthvyas7
+
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
